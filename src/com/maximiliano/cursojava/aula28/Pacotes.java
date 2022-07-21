7414e95e7c835a8865dc8a7965150599229e322e
maxmillionsmax <maximilianocaetano@gmail.com>
2022-07-21 01:10:22 -0300

    Pacotes e import

---
+++ src/com/maximiliano/cursojava/aula28/Pacotes.java
@@ -0,0 +1,21 @@
+package com.maximiliano.cursojava.aula28;
+
+import com.maximiliano.cursojava.aula20.labs.Exer05;
+import com.maximiliano.cursojava.aula20.labs.Exer06;
+import com.maximiliano.cursojava.aula27.Carro;
+
+
+
+public class Pacotes {
+
+	public static void main(String[] args) {
+		
+		Carro carro;
+		Carro carro2;
+		
+		Exer06 exer06;
+		Exer05 exer05;
+
+	}
+
+}
