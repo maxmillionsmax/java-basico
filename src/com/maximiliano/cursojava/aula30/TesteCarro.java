3654ad4d78a6b9ca325239b65a12088d737e813e
maxmillionsmax <maximilianocaetano@gmail.com>
2022-07-21 01:51:03 -0300

    Palavra THIS e uso

---
+++ src/com/maximiliano/cursojava/aula30/TesteCarro.java
@@ -0,0 +1,13 @@
+package com.maximiliano.cursojava.aula30;
+
+public class TesteCarro {
+
+	public static void main(String[] args) {
+		
+        Carro van = new Carro("Fiat", "Ducta");
+						
+		System.out.println(van.numPassageiros);
+
+	}
+
+}
