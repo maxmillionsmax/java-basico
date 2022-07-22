17cef97ba113fb9e7490587861d3ce9e2f3dc839
maxmillionsmax <maximilianocaetano@gmail.com>
2022-07-22 01:06:20 -0300

    Exemplo Calculadora Ori-Obj

---
+++ src/com/maximiliano/cursojava/aula34/labs/Exer02.java
@@ -0,0 +1,18 @@
+package com.maximiliano.cursojava.aula34.labs;
+
+public class Exer02 {
+
+	public static void main(String[] args) {
+		
+		imprimirTela(Calculadora.somar(2, 2));
+		imprimirTela(Calculadora.subtrair(2, 2));
+		imprimirTela(Calculadora.multiplicar(2,2));
+		imprimirTela(Calculadora.dividir(2,2));
+		imprimirTela(Calculadora.potencia(2,2));
+		
+	}
+
+	static void imprimirTela(int num) {
+		System.out.println(num);
+	}
+}
