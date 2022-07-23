5de851c51031bd8d2b1b886cdf5341f4721a2366
maxmillionsmax <maximilianocaetano@gmail.com>
2022-07-22 22:15:32 -0300

    Palavra chave SUPER

---
+++ src/com/maximiliano/cursojava/aula38/Teste.java
@@ -0,0 +1,22 @@
+package com.maximiliano.cursojava.aula38;
+
+public class Teste {
+
+	public static void main(String[] args) {
+
+		/*Aluno aluno = new Aluno();
+
+		Professor professor = new Professor();
+		
+		Pessoa pessoa = new Pessoa();
+		
+		aluno.setNome("Maria");
+		*/
+		
+		Pessoa aluno = new Aluno();
+		//aluno.set
+		Pessoa professor = new Professor();
+		//professor.setNome(null);
+	}
+
+}
